b355f89c51e20a5bed44468343442a949d8f2b53
Marcel <MTRNord@users.noreply.github.com>
2015-10-29 13:03:50 +0100

    Update FFNORDController.php

--- src/Puphpet/MainBundle/Controller/FFNORDController.php
+++ src/Puphpet/MainBundle/Controller/FFNORDController.php
@@ -1,1 +1,42 @@
-
+<?php
+namespace Puphpet\MainBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+class ApacheController extends Controller
+{
+    public function indexAction(array $data)
+    {
+        return $this->render('PuphpetMainBundle:ffnord:form.html.twig', [
+            'apache' => $data,
+        ]);
+    }
+    public function vhostAction()
+    {
+        return $this->render('PuphpetMainBundle:ffnord/sections:vhost.html.twig', [
+            'vhost' => $this->getData()['empty_vhost'],
+        ]);
+    }
+    public function directoryAction(Request $request)
+    {
+        return $this->render('PuphpetMainBundle:ffnord/sections:directory.html.twig', [
+            'vhostId'   => $request->get('vhostId'),
+            'directory' => $this->getData()['empty_directory'],
+        ]);
+    }
+    public function filesMatchAction(Request $request)
+    {
+        return $this->render('PuphpetMainBundle:ffnord/sections:filesMatch.html.twig', [
+            'vhostId'     => $request->get('vhostId'),
+            'directoryId' => $request->get('directoryId'),
+            'filesMatch'  => $this->getData()['empty_files_match'],
+        ]);
+    }
+    /**
+     * @return array
+     */
+    private function getData()
+    {
+        $manager = $this->get('puphpet.extension.manager');
+        return $manager->getExtensionAvailableData('ffnord');
+    }
+}
